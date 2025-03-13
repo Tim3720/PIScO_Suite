b2242c827088aeb36055eca7bb3bc8793b93bf50
Tim3720 <timkalsb@gmail.com>
2025-03-13 12:15:58 +0100

    torch test

--- Tests/crop_spline.py
+++ Tests/crop_spline.py
@@ -7,7 +7,7 @@ import scipy.interpolate as interpolate
 # path = "/home/tim/Documents/ArbeitTestData/TestResultsCrops/Crops"
 # files = os.listdir(path)
 # file = os.path.join(path, np.random.choice(files))
-file = "/home/tim/Documents/ArbeitTestData/TestResultsCrops/Crops/CUSCO-Peru-2020_M4_PISCO20200315_155239.4700_1.9bar_223.png"
+file = "/home/tim/Documents/ArbeitTestData/TestResultsCrops/Crops/SO308_1-5-1_PISCO2_0012.81dbar-20.10S-036.10E-26.08C_20241106-15340569_0.png"
 print(file)
 img = cv.imread(file)
 
@@ -22,29 +22,30 @@ plt.imshow(img[:, :, 2], cmap="inferno")
 plt.show()
 
 # # img = img[:, :, 0]
-# contour = img[:, :, 1].astype(np.float64) + img[:, :, 2].astype(np.float64) * 255
-# print(contour.shape)
-# # plt.imshow(contour)
-#
-# cnt = np.where(contour > 0)
-# cnt = list(np.array(cnt).T)
-# cnt = np.array(sorted(cnt, key=lambda x: contour[x[0], x[1]]))
-# cnt = cnt[::3]
+contour = img[:, :, 1].astype(np.float64) + img[:, :, 2].astype(np.float64) * 255
+
+# plt.imshow(contour)
+# plt.plot(contour[:, 0], contour[:, 1])
+cnt = np.where(contour > 0)
+cnt = list(np.array(cnt).T)
+cnt = np.array(sorted(cnt, key=lambda x: contour[x[0], x[1]]))
+cnt = cnt[::3]
 # # print(cnt)
 #
 #
 # # img = img[:, :, 2]
-# x = cnt[:, 1]
-# y = cnt[:, 0]
+x = cnt[:, 1]
+y = cnt[:, 0]
 #
 # # # print(CubicSpline(x, y))
-# tck, u = interpolate.splprep([x, y], per=True, s=0)
-# print(tck)
-# x_i, y_i = interpolate.splev(np.linspace(0, 1, 1000), tck)
-# # plt.scatter(x, y, zorder=3)
-# plt.plot(x_i, y_i)
+tck, u = interpolate.splprep([x, y], per=True, s=0)
+print(tck)
+x_i, y_i = interpolate.splev(np.linspace(0, 1, 1000), tck)
+plt.scatter(x, y, zorder=3)
+plt.plot(x_i, y_i)
 # #
-# plt.show()
+plt.imshow(img[:,:,0], origin="lower")
+plt.show()
 #
 # # img = cv.resize(img, (0, 0), fx=5, fy=5)
 # # cv.imshow("img", img)
--- Tests/flake.nix
+++ Tests/flake.nix
@@ -5,9 +5,13 @@ inputs = {
         url = "github:numtide/flake-utils";
         inputs.system.follows = "systems";
     };
+    custom_libtorch = {
+        url = "github:pytorch/pytorch";
+        flake = false;
+    };
 };
 
-outputs = { self, nixpkgs, flake-utils, ... }:
+outputs = { self, nixpkgs, flake-utils, ... } @inputs:
     flake-utils.lib.eachDefaultSystem
     (system:
     let
@@ -17,28 +21,44 @@ outputs = { self, nixpkgs, flake-utils, ... }:
         };
         libs = [
             pkgs.libcxx
-            pkgs.eigen
+            pkgs.libtorch-bin
         ];
+        libtorch = pkgs.callPackage ./. {
+            inherit (inputs) custom_libtorch;
+        };
     in
         with pkgs;
             {
             devShells.default = mkShell {
+        # default = libtorch;
                 nativeBuildInputs = with pkgs; [
+                    libtorch
                     bashInteractive
-                    eigen
                     libcxx
                     pkg-config
                     gcc
+                    cmake
                 ];
                 buildInputs = with pkgs; [
-                    (python312.withPackages(ps: with ps;[
-                        (opencv4.override {enableGtk2 = true;})
+                    (python311.withPackages(ps: with ps;[
+                        # (opencv4.override {enableGtk2 = true;})
                         numpy
                         scipy
                         matplotlib
+                        pytorch-bin
                     ]))
+                    cudatoolkit
+                    # (libtorch-bin.override ({
+                    # }))
+                    (opencv.override ({
+                        enableGtk3 = true;
+                        enableCuda = false;
+                        enableFfmpeg = true;
+                        enableUnfree = true;
+                    }))
                 ];
                 shellHook = ''
+                    echo $(inputs.custom_libtorch)
                 '';
                 LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libs}";
             };
