29e71f63f4acf2160f9f7b6e38ed3c47afe77a4b
Tim3720 <timkalsb@gmail.com>
2025-03-14 13:31:46 +0100

    torch test

--- Tests/flake.nix
+++ Tests/flake.nix
@@ -1,13 +1,9 @@
 {
 inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils = {
         url = "github:numtide/flake-utils";
         inputs.system.follows = "systems";
-    };
-    custom_libtorch = {
-        url = "github:pytorch/pytorch";
-        flake = false;
     };
 };
 
@@ -18,48 +14,32 @@ outputs = { self, nixpkgs, flake-utils, ... } @inputs:
         pkgs = import nixpkgs {
             inherit system;
             config.allowUnfree = true;
+            config.cudaSupport = true;
         };
         libs = [
             pkgs.libcxx
-            pkgs.libtorch-bin
         ];
-        libtorch = pkgs.callPackage ./. {
-            inherit (inputs) custom_libtorch;
-        };
+
+        custom_libtorch = pkgs.callPackage ./default.nix {};
     in
         with pkgs;
             {
             devShells.default = mkShell {
-        # default = libtorch;
                 nativeBuildInputs = with pkgs; [
-                    libtorch
                     bashInteractive
                     libcxx
                     pkg-config
-                    gcc
-                    cmake
-                ];
-                buildInputs = with pkgs; [
-                    (python311.withPackages(ps: with ps;[
-                        # (opencv4.override {enableGtk2 = true;})
-                        numpy
-                        scipy
-                        matplotlib
-                        pytorch-bin
-                    ]))
                     cudatoolkit
-                    # (libtorch-bin.override ({
-                    # }))
-                    (opencv.override ({
-                        enableGtk3 = true;
-                        enableCuda = false;
-                        enableFfmpeg = true;
-                        enableUnfree = true;
-                    }))
                 ];
+                buildInputs = [
+                    pkgs.python3Packages.torch
+                    (pkgs.python3Packages.opencv4.override { enableGtk3 = true; })
+                    custom_libtorch
+                ] ;
+
                 shellHook = ''
-                    echo $(inputs.custom_libtorch)
                 '';
+                    # echo ${pkgs.libtorch-bin}
                 LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libs}";
             };
         }
