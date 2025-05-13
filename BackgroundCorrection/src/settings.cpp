a418d2516b5cd5fb634d666e02671f0515bc9d1e
Tim3720 <timkalsb@gmail.com>
2025-05-13 17:28:11 +0200

    Merge

--- BackgroundCorrection/src/settings.cpp
+++ BackgroundCorrection/src/settings.cpp
@@ -1,5 +1,7 @@
 #include "settings.hpp"
 #include "background_correction.hpp"
+#include "error.hpp"
+#include "parser.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
 /// External settings:
@@ -25,4 +27,45 @@ void defaultSettings()
     e_useMultiChannelSaveMode = true;
 }
 
-void loadSettings(char* filename) { }
+void loadSettings(char* filename)
+{
+    std::unordered_map<std::string, std::string> settings = parseConfigFile(filename);
+
+    readParameterString(settings, e_sourcePath, "sourcePath");
+    if (e_sourcePath.back() != '/')
+        e_sourcePath += "/";
+
+    readParameterString(settings, e_savePath, "savePath");
+    if (e_savePath.back() != '/')
+        e_savePath += "/";
+
+    std::string colorModeHelper;
+    readParameterString(settings, colorModeHelper, "colorMode");
+    if (colorModeHelper == "gray")
+        e_colorMode = cv::IMREAD_GRAYSCALE;
+    else if (colorModeHelper == "rgb")
+        e_colorMode = cv::IMREAD_COLOR;
+    else
+        throw std::runtime_error(
+            makeRed("Invalid value for setting colorMode: " + colorModeHelper));
+    // TODO: Implement single/multi channel mode
+
+    readParameterInt(settings, e_nBackgroundImages, "nBackgroundImages");
+
+    std::string bgCorrectionModelHelper;
+    readParameterString(settings, bgCorrectionModelHelper, "backgroundCorrectionModel");
+    if (bgCorrectionModelHelper == "minMethod")
+        e_backgroundCorrectionModel = minMethod;
+    else if (bgCorrectionModelHelper == "minMaxMethod")
+        e_backgroundCorrectionModel = minMaxMethod;
+    else if (bgCorrectionModelHelper == "averageMethod")
+        e_backgroundCorrectionModel = averageMethod;
+    else if (bgCorrectionModelHelper == "medianMethod")
+        e_backgroundCorrectionModel = medianMethod;
+    else
+        throw std::runtime_error(
+            makeRed("Invalid value for setting backgroundCorrectionModel: " +
+                    bgCorrectionModelHelper));
+
+    readParameterBool(settings, e_useMultiChannelSaveMode, "useMultiChannelSaveMode");
+}
